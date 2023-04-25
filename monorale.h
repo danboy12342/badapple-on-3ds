39dafbeadd3701cc36e80759829efae741f5f83e
danboy12342 <58471239+danboy12342@users.noreply.github.com>
2023-04-25 11:14:53 +0100

    Add files via upload

---
+++ monorale.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <3ds.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern volatile u32 runVideo, playVideo; //important
+
+typedef struct {
+	uint32_t offset;
+	uint32_t cmdcnt;
+} __attribute__((packed)) monorale_frameinf;
+
+typedef struct {
+	uint32_t framecnt;
+	monorale_frameinf inf[0];
+} __attribute__((packed)) monorale_hdr;
+
+static inline size_t monorale_frames(monorale_hdr *hdr) {
+	return hdr->framecnt;
+}
+
+monorale_hdr *monorale_init(const char *path);
+void monoraleThread(void *arg);
---
+++ sndogg.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <3ds.h>
+
+#include <string.h>
+
+#include <tremor/ivorbiscodec.h>
+#include <tremor/ivorbisfile.h>
+
+extern volatile u32 runSound, playSound;
+
+Result initSound(u16 sample_rate);
+
+int initVorbis(const char *path, OggVorbis_File *vorbisFile, vorbis_info **vi);
+void soundThread(void *arg);
+//this is a little silly but it runs!
